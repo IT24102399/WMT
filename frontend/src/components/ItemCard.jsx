3614ce261f942063a07f7c5369e7e748ecb1d4a9
IT24102399 <IT24102399@my.sliit.lk>
2026-04-28 11:49:05 +0530

    Update ItemCard.jsx

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -11,8 +11,8 @@ function ItemCard({ item, onDelete }) {
       <h3>{item.name}</h3>
       <p><strong>Category:</strong> {item.category}</p>
       <p><strong>Price:</strong> ${item.price}</p>
-      <p><strong>WarrantyPeriod:</strong> ${item.WarrantyPeriod}</p>
       <p>{item.description}</p>
+      <p><strong>WarrantyPeriod:</strong> ${item.WarrantyPeriod}</p>
 
       <div className="card-actions">
         <Link className="btn secondary" to={`/edit-item/${item._id}`}>Edit</Link>
